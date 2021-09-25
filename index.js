21c873ea9f50aafb1d1d3ba66248836868ebea7b
sauron-00 <sauron.00@hotmail.com>
2021-09-25 14:10:17 +0200

    recreate npm project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+    $("button").click(hideBtn)
+})
+
+
+function hideBtn(){
+    $("p").hide()
+}
+
+
 console.log("Hello World")
+
+var textElemnt = document.getElementById("txt")
+
+textElemnt.innerHTML = "HELLO";
